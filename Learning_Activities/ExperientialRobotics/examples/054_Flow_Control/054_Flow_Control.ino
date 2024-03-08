9abdf66d802466d04a9f9d69d2616e35b87ff053
David Rihtarsic <david.rihtarsic@gmail.com>
2024-03-08 08:44:39 +0100

    flow controlling

---
+++ Learning_Activities/ExperientialRobotics/examples/054_Flow_Control/054_Flow_Control.ino
@@ -0,0 +1,21 @@
+#include "RobotMovingFunctions.h"
+
+void setup()
+{
+  setIOpins();
+
+repeating_moves:
+  moveForeward();
+  delay(1000);
+  moveLeft();
+  delay(550);
+  robotStop();
+  delay(1000);
+goto repeating_moves;
+
+}
+
+void loop()
+{
+
+}
